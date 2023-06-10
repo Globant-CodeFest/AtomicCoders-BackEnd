f53b06debd40655e01be4c36a638d735bfa5d0ff
alanmartinc <alancabot.ac@gmail.com>
2023-06-10 15:54:56 -0300

    prompt v2

--- prompt/prompt.py
+++ prompt/prompt.py
@@ -1,27 +1,29 @@
 import requests
 
-# Define los parámetros de la solicitud
-url = "https://api.openai.com/v1/chat/completions"
-headers = {
-    "Authorization": "Bearer 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",
-    "Content-Type": "application/json"
-}
-data = {
-    "model": "gpt-3.5-turbo",
-    "messages": [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-    ]
-}
+message = "tengo 2000000 EUR quiero al delantero mas veloz para mi equipo?"
+def chat_with_gpt(message):
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Authorization": "Bearer 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",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message}
+        ]
+    }
 
-# Envía la solicitud POST a la API
-response = requests.post(url, headers=headers, json=data)
-response_data = response.json()
+    response = requests.post(url, headers=headers, json=data)
+    response_data = response.json()
 
-# Procesa la respuesta
-for message in response_data["choices"][0]["message"]["content"]:
-    role = message["role"]
-    content = message["content"]
-    print(f"{role}: {content}")
+    assistant_response = response_data["choices"][0]["message"]["content"]
+    return assistant_response
+
+# Ejemplo de uso
+api_key = "YOUR_API_KEY"
+user_input = input("User: ")
+
+response = chat_with_gpt(user_input)
+print("ChatGPT:", response)
