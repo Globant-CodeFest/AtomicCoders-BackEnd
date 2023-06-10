93adb3788b196f5d7b383c56005ab0b8a4387fbf
Jonathan <jonathanalexismontenegro97@gmail.com>
2023-06-10 17:12:54 -0300

    Integración del prompt

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import logging
 
+import requests
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -7,34 +8,44 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 app.logger.setLevel(logging.DEBUG)
 
-def process_question(clientQuestionQuery):
 
-    return [{
-        'sofifa_id': 1,
-        'short_name': 'Leonel Messi',
-        'age': 32,
-        'nationality_name': 'Argentina',
-        'value_eur': 100000000,
-        'wage_eur': 1000000,
-        'player_positions': ['RW', 'ST', 'CF'],
-        'club_name': 'Barcelona',
-        'player_face_url': 'https://cdn.sofifa.org/players/4/19/158023.png',
-        'pace': 87.1,
-        'passing': 90.0,
-        'shooting': 95.0,
-        'dribbling': 96.0,
-        'defending': 32.0,
-        'physic': 61.0
-    }]
+def chat_with_gpt(message):
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Authorization": "Bearer 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",
+        "Content-Type": "application/json"
+    }
+
+    question = "Desde ahora eres un asistente virtual que responde preguntas sobre el FIFA 21 \ " \
+               "Vas a identificar los siguientes items de un texto: \ " \
+               " - Valor económico del jugador (devolver un valor numérico)" \
+               " - Posiciones del jugador buscado (una lista que devuelve uno o más de los siguientes valores: delantero, centrocampista, defensor, arquero)" \
+               "El texto está delimitado en \"\"\" \"\"\"." \
+               "El formato de respuesta en JSON sin salto de líneas con las claves \"valor\", \"posiciones\"." \
+               "Si la información no está presente devolver el valor vacío." \
+               " Texto: \"\"\""+message+"\"\"\"\""
+
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {"role": "system", "content": question},
+        ]
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    response_data = response.json()
+
+    messages = response_data["choices"][0]["message"]["content"]
+    return messages
+
 
 @app.route('/api/v1/guru-pront', methods=['GET'])
 def get_result_pront():
+    client_question_query = request.args.get('client-question')
 
-    clientQuestionQuery = request.args.get('client-question')
+    app.logger.debug('Client question: %s', client_question_query)
 
-    app.logger.debug('Client question: %s', clientQuestionQuery)
-
-    result = process_question(clientQuestionQuery)
+    result = chat_with_gpt(client_question_query)
 
     return jsonify(result)
 
